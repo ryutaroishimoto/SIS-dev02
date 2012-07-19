d17c1f3e7dd2dfd4d60e62b4c521805a43350ce8
Tetsuya Ohta <ohta@raw-hide.co.jp>
2012-07-19 14:52:18 +0900

    remove code

--- sample/mobileclip_exchange/dto_mobileclip.py
+++
@@ -1,6 +0,0 @@
-#coding: utf-8
-# package mobileclip_exchange.dto_mobileclip
-# makes mobileclip rss ( formatted for RSS2)
-class DTOMobileclip :
-  def __init__(self, opts=[]) :
-    self.title = ""
--- sample/mobileclip_exchange/fifa/parser.py
+++
@@ -1,9 +0,0 @@
-#coding:utf-8
-# package: mobileclip.fifa.parser
-# author: ohta@RHD
-# created: 2012-07-16
-class Parser:
-  def __init__(self) :
-    print "included"
-  def parse(self) :
-    print "parse"
